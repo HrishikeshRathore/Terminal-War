4f02f5bc3c03fda6818304534ee1e31a761f5700
Hrishikesh Rathore <hrishikeshrathore7@gmail.com>
2021-04-18 11:53:47 +0530

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -11,42 +11,6 @@ public class Main {
         GameManager gameManager = new GameManager();
 
         gameManager.start();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//        Heroes.addHeroes();
-//	// write your code here
-//        List<Card> listOfHeroes = Heroes.getHeroes();
-//
-//        for(int i=0; i<listOfHeroes.size(); i++){
-//            System.out.println(listOfHeroes.get(i).getCardData());
-//        }
-//
-//        Player player = new Player("Hrishikesh");
-//
-//        player.addCard(5);
-//        player.addCard(2);
-//        player.addCard(1);
-//
-//        player.displayPlayerCards();
-
+        
     }
 }
